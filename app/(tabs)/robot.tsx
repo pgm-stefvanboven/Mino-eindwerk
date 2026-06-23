6eb38ecb4dbc41f3909161d881c3eb009d11c0fc
pgm-stefvanboven <stef.vanboven@student.arteveldehs.be>
2026-06-23 10:55:01 +0200

    updated IP-adress PI connection

--- app/(tabs)/robot.tsx
+++ app/(tabs)/robot.tsx
@@ -17,8 +17,8 @@ import { useSafeAreaInsets } from "react-native-safe-area-context";
 import * as ScreenOrientation from "expo-screen-orientation";
 import * as NavigationBar from "expo-navigation-bar";
 
-const VIDEO_IP = "http://10.250.156.75:5001";
-const COMMAND_IP = "http://10.250.156.75:5002";
+const VIDEO_IP = "http://10.31.194.75:5001";
+const COMMAND_IP = "http://10.31.194.75:5002";
 
 // --- THEMA ---
 const THEME = {
